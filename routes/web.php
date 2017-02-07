80f77b8a2d997c4ceb25c9bea86052ad608e319b
jorge <tech@superfuds.com.co>
2017-02-07 12:16:47 -0500

    add new controllers

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/home', 'HomeController@index');
 Route::get('/dash', 'DashboardController@index');
 
 Route::resource('/products', 'ProductController');
+Route::resource('/entry', 'EntryController');
+Route::resource('/departure', 'DepartureController');

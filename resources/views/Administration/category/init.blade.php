c02c8d85b1f311b50fef3c817d6c818c367536d8
jorge <tech@superfuds.com.co>
2017-04-20 10:22:29 -0500

    change name folder

--- resources/views/Administration/category/init.blade.php
+++ resources/views/Administration/category/init.blade.php
@@ -35,6 +35,6 @@
         </div>
     </div>
 </div>
-@include('Administration.Category.form')
+@include('Administration.category.form')
 {!!Html::script('js/Administration/Category.js')!!}
 @endsection

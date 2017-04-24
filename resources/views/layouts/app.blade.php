2ed7b17cc201a85831ca006bbd419b4edfe43134
jorge <tech@superfuds.com.co>
2017-04-24 14:58:46 -0500

    change main title in form login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'SUperfuds') }}</title>
+    <title>Superfus(Jelti)</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">

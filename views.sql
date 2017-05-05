b30c6bb0de18e4566a59f25e559359aea4bfbe75
jorge <tech@superfuds.com.co>
2017-05-05 12:46:56 -0500

    fixed view

--- views.sql
+++ views.sql
@@ -30,13 +30,14 @@ LEFT JOIN parameters as status ON status.code=p.status_id and status."group"='ge
 
 
 create view vdepartures as 
-            select d.id,d.created_at, s.name || s.last_name as client,w.description as warehouse,
-            c.description as city,p.description status,d.status_id,d.responsible_id
+            select d.id,d.created_at, coalesce(s.business_name,'') as client,w.description as warehouse,
+            c.description as city,p.description status,d.status_id,d.responsible_id,u.name ||' '|| u.last_name as responsible
             from departures d
-            JOIN stakeholder s ON s.id = d.client_id
+            JOIN branch_office s ON s.id = d.client_id
             JOIN warehouses w ON w.id = d.warehouse_id
             JOIN cities c ON c.id = d.city_id
             JOIN parameters p ON p.id = d.status_id
+            JOIN users u ON u.id = d.responsible_id
             WHERE p.group='entry'
 
 create view vcities as 

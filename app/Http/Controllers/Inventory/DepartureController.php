327f15e95bf5a9d97af605fc6589b9153ff3288c
jorge <tech@superfuds.com.co>
2017-05-16 17:20:13 -0500

    improve

--- app/Http/Controllers/Inventory/DepartureController.php
+++ app/Http/Controllers/Inventory/DepartureController.php
@@ -100,7 +100,7 @@ class DepartureController extends Controller {
         $sale = Sales::where("departure_id", $id)->first();
         $detail = DB::table("sales_detail")
                 ->select("quantity", DB::raw("sales_detail.tax * 100 as tax"), DB::raw("coalesce(sales_detail.description,'') as description"), 
-                        "products.title as product", "products.id as product_id", "sales_detail.value", "products.units_sf", 
+                        "products.title as product", "products.id as product_id", "sales_detail.value", "sales_detail.units_sf", 
                         DB::raw("sales_detail.units_sf * sales_detail.quantity as quantityTotal"), 
                         DB::raw("sales_detail.value * sales_detail.quantity * sales_detail.units_sf as valueTotal"))
                 ->join("products", "sales_detail.product_id", "products.id")

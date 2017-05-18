0b7afbf8c6b677982a4e0a96433c9961fef8928e
jorge <tech@superfuds.com.co>
2017-05-18 12:47:59 -0500

    add

--- app/Http/Controllers/Inventory/DepartureController.php
+++ app/Http/Controllers/Inventory/DepartureController.php
@@ -147,7 +147,7 @@ class DepartureController extends Controller {
                 ->where("sale_id", $sale["id"])
                 ->orderBy("order", "asc")
                 ->get();
-        
+
         $dep = Departures::find($id);
         $cli = Branch::where("stakeholder_id", $sale["client_id"])->first();
         $user = Users::find($sale["responsible_id"]);
@@ -185,8 +185,8 @@ class DepartureController extends Controller {
         $totalWithTax = $totalSum + $totalTax19 + $totalTax5 + $dep->shipping_cost;
 
         $tool = new ToolController();
-        
 
+        $cli["business_name"]=$this->cleanText($cli["business_name"]);
         $data = [
             'rete' => 0,
 //            'rete' => $rete["value"],
@@ -202,17 +202,62 @@ class DepartureController extends Controller {
             'totalWithTax' => "$ " . number_format(($totalWithTax), 2, ',', '.'),
             'shipping' => "$ " . number_format(($dep->shipping_cost), 2, ',', '.'),
             'invoice' => $dep->invoice,
-            'textTotal'=>trim($tool->to_word($totalWithTax))
+            'textTotal' => trim($tool->to_word($totalWithTax))
         ];
 
         $pdf = \PDF::loadView('Inventory.departure.pdf', [], $data, [
                     'title' => 'Invoice']);
 
-        $pdf2 = \PDF::loadView('Inventory.departure.pdf', [], $data, [
-                    'title' => 'Invoice']);
-
-        $pdf2->stream('document.pdf');
         return $pdf->stream('document.pdf');
+    }
+
+    function cleanText($string) {
+        $string = trim($string);
+        $string = utf8_encode((filter_var($string, FILTER_SANITIZE_STRING)));
+        $string = str_replace(
+                array('á', 'à', 'ä', 'â', 'ª', 'Á', 'À', 'Â', 'Ä', 'Ã'), array('a', 'a', 'a', 'a', 'a', 'A', 'A', 'A', 'A', 'A'), $string
+        );
+        $string = str_replace(
+                array('é', 'è', 'ë', 'ê', 'É', 'È', 'Ê', 'Ë'), array('e', 'e', 'e', 'e', 'E', 'E', 'E', 'E'), $string
+        );
+        $string = str_replace(
+                array('í', 'ì', 'ï', 'ï', 'î', 'Í', 'Ì', 'Ï', 'Î'), array('i', 'i', 'i', 'i', 'i', 'I', 'I', 'I', 'I'), $string
+        );
+        $string = str_replace(
+                array('ó', 'ò', 'ö', 'ô', 'Ó', 'Ò', 'Ö', 'Ô'), array('o', 'o', 'o', 'o', 'O', 'O', 'O', 'O'), $string
+        );
+        $string = str_replace(
+                array('ú', 'ù', 'ü', 'û', 'Ú', 'Ù', 'Û', 'Ü'), array('u', 'u', 'u', 'u', 'U', 'U', 'U', 'U'), $string
+        );
+        $string = str_replace(
+                array('ñ', 'Ñ', 'ç', 'Ç'), array('n', 'N', 'c', 'C'), $string
+        );
+        $string = str_replace(
+                array("\\", "¨", "º", "–", "~", "|", "·",
+            "¡", "[", "^", "`", "]", "¨", "´", "¿",
+            '§', '¤', '¥', 'Ð', 'Þ'), '', $string
+        );
+        $string = str_replace(
+                array(";",), array(","), $string
+        );
+        $string = str_replace(
+                array("&#39;", "&#39,", '&#34;', '&#34,'), array("'", "'", '"', '"'), $string
+        );
+        $string = htmlentities($string, ENT_QUOTES | ENT_IGNORE, 'UTF-8');
+
+        $string = str_replace(
+                array('&quot;', '&#39;', '&#039;'), array('"', "'", "'"), $string
+        );
+        $string = str_replace(
+                array('&amp;', '&nbsp;'), array('&', ' '), $string
+        );
+        $string = str_replace(
+                array('&deg;', '&sup3;', '&shy;'), array(''), $string
+        );
+        $string = str_replace(
+                array('&copy;', '&sup3;', '&shy;', '&plusmn;'), array('e', 'o', 'i', 'n'), $string
+        );
+        return $string;
     }
 
     public function dateText() {

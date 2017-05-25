d99a067204eb9c62b728e81c07706499791e5db7
jorge <tech@superfuds.com.co>
2017-05-24 19:54:31 -0500

    add changes

---
+++ resources/views/Sales/Purse/list.blade.php
@@ -0,0 +1,43 @@
+<div class="col-md-12">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <div class="row">
+                <div class="col-lg-3">List Departure</div>
+            </div>
+
+        </div>
+        <div class="panel-body">
+            <table class="table table-condensed  table-hover" id="tbl" width='100%'>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Consecutive</th>
+                        <th>Invoice</th>
+                        <th>Date</th>
+                        <th>Client</th>
+                        <th>Commercial</th>
+                        <th>Warehouse</th>
+                        <th>City</th>
+                        <th>Status</th>
+                        <th tipo="prueba">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <th></th>
+                        <th>Consecutive</th>
+                        <th>Date</th>
+                        <th>Client</th>
+                        <th>Commercial</th>
+                        <th>Warehouse</th>
+                        <th>City</th>
+                        <th>Status</th>
+                        <th></th>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</div>

7e1ead5efedbabe2d29482bb6abc2858e614f029
jorge <tech@superfuds.com.co>
2017-06-07 18:14:25 -0500

    add

---
+++ resources/views/Inventory/departure/pdffootercredit.blade.php
@@ -0,0 +1,69 @@
+<table width='100%' bgcolor="#FAFAFA">
+    <tr>
+        <td width='20%' >Observaciones</td>
+        <td width='30%'><?php echo $client["observations"]; ?></td>
+        <td>
+            <table>
+                <tr>
+                    <td width='240px' >Total Factura</td>
+                    <td><?php echo $totalInvoice; ?></td>
+                </tr>
+<!--                <tr>
+                    <td>Descuento</td>
+                    <td>$ 0</td>
+                </tr>-->
+                <tr>
+                    <td>Flete</td>
+                    <td><?php echo $shipping; ?></td>
+                </tr>
+                <?php
+                if ($tax5num > 0) {
+                    ?>
+                    <tr>
+                        <td>Iva 5%</td>
+                        <td><?php echo $tax5; ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+                <?php
+                if ($tax19num > 0) {
+                    ?>
+                    <tr>
+                        <td>Iva 19%</td>
+                        <td><?php echo $tax19; ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+
+                <?php
+                if ($rete > 0) {
+                    ?>
+                    <tr>
+                        <td>Retefuente</td>
+                        <td><?php echo $formatRete; ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+
+                <tr>
+                    <td>Exento</td>
+                    <td><?php echo $exept; ?></td>
+                </tr>
+                <tr>
+                    <td><b>Monto</b></td>
+                    <td><b><?php echo $totalWithTax; ?></b></td>
+                </tr>
+
+            </table>
+        </td>
+    </tr>
+
+</table>
+<table>
+    <tr>
+        <td><?php echo $textTotal; ?></td>
+    </tr>
+</table>

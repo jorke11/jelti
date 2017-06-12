de01b3c553ee27455bde88945616870cc1e7cb4c
jorge <tech@superfuds.com.co>
2017-06-12 10:38:30 -0500

    ad

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,57 +29,55 @@ class HomeController extends Controller {
      * @return \Illuminate\Http\Response
      */
     public function index() {
-//        $sql = "
-//            SELECT p.title,sum(d.quantity) cantidadTotal,round(sum(d.value * d.quantity * d.units_sf)) as total
-//            FROM sales_detail d
-//            JOIN sales s ON s.id=d.sale_id  
-//            JOIN products p ON p.id=d.product_id  
-//            WHERE product_id IS NOT NULL AND s.created BETWEEN '" . date("Y-m") . "-01 00:00' and '" . date("Y-m-d") . " 23:59'
-//            GROUP BY 1
-//            ORDER BY 2 DESC LIMIT 1";
-//        $product = DB::select($sql);
-//        $product = $product[0];
-        $product =  array();
-//
-//        $sql = "
-//            SELECT cli.business,sum(d.quantity) cantidadtotal,sum(d.value*d.quantity*d.units_sf) total
-//            FROM sales_detail d
-//            JOIN sales s ON s.id=d.sale_id
-//            JOIN departures dep ON dep.id=s.departure_id
-//            JOIN stakeholder cli ON cli.id=s.client_id
-//            WHERE product_id IS NOT NULL AND s.created BETWEEN '" . date("Y-m") . "-01 00:00' and '" . date("Y-m-d") . " 23:59'
-//            GROUP BY 1
-//            ORDER BY 2 DESC LIMIT 1";
-//
-        $client = array();
-//        $client = DB::select($sql);
-//        $client = $client[0];
-//
-//        $sql = "
-//            SELECT s.business proveedor,round(sum(d.quantity*d.units_sf)) cantidadtotal,round(sum(d.value * d.quantity * d.units_sf)) total
-//            FROM sales_detail d
-//            JOIN sales sal ON sal.id=d.sale_id  
-//            JOIN products p ON p.id=d.product_id  
-//            JOIN stakeholder s ON s.id=p.supplier_id  
-//            WHERE product_id is not null AND sal.created BETWEEN '" . date("Y-m") . "-01 00:00' and '" . date("Y-m-d") . " 23:59'
-//            GROUP BY 1
-//            ORDER BY 3 desc LIMIT 1";
-//
-//        $supplier = DB::select($sql);
-//        $supplier = $supplier[0];
-        $supplier =  array();
-//
-//        $sql = "
-//            SELECT u.name ||' '|| u.last_name as vendedor,sum(d.quantity) cantidadtotal,round(sum(d.value * d.quantity * d.units_sf)) total
-//            FROM sales_detail d
-//            JOIN sales s ON s.id=d.sale_id
-//            JOIN users u ON u.id=s.responsible_id
-//            GROUP BY 1
-//            ORDER BY 3 desc";
-//
-//        $commercial = DB::select($sql);
-//        $commercial = $commercial[0];
-        $commercial =  array();
+        $sql = "
+            SELECT p.title,sum(d.quantity) cantidadTotal,round(sum(d.value * d.quantity * d.units_sf)) as total
+            FROM sales_detail d
+            JOIN sales s ON s.id=d.sale_id  
+            JOIN products p ON p.id=d.product_id  
+            WHERE product_id IS NOT NULL AND s.created BETWEEN '" . date("Y-m") . "-01 00:00' and '" . date("Y-m-d") . " 23:59'
+            GROUP BY 1
+            ORDER BY 2 DESC LIMIT 1";
+        $product = DB::select($sql);
+        $product = $product[0];
+        
+
+        $sql = "
+            SELECT cli.business,sum(d.quantity) cantidadtotal,sum(d.value*d.quantity*d.units_sf) total
+            FROM sales_detail d
+            JOIN sales s ON s.id=d.sale_id
+            JOIN departures dep ON dep.id=s.departure_id
+            JOIN stakeholder cli ON cli.id=s.client_id
+            WHERE product_id IS NOT NULL AND s.created BETWEEN '" . date("Y-m") . "-01 00:00' and '" . date("Y-m-d") . " 23:59'
+            GROUP BY 1
+            ORDER BY 2 DESC LIMIT 1";
+
+    
+        $client = DB::select($sql);
+        $client = $client[0];
+
+        $sql = "
+            SELECT s.business proveedor,round(sum(d.quantity*d.units_sf)) cantidadtotal,round(sum(d.value * d.quantity * d.units_sf)) total
+            FROM sales_detail d
+            JOIN sales sal ON sal.id=d.sale_id  
+            JOIN products p ON p.id=d.product_id  
+            JOIN stakeholder s ON s.id=p.supplier_id  
+            WHERE product_id is not null AND sal.created BETWEEN '" . date("Y-m") . "-01 00:00' and '" . date("Y-m-d") . " 23:59'
+            GROUP BY 1
+            ORDER BY 3 desc LIMIT 1";
+
+        $supplier = DB::select($sql);
+        $supplier = $supplier[0];
+
+        $sql = "
+            SELECT u.name ||' '|| u.last_name as vendedor,sum(d.quantity) cantidadtotal,round(sum(d.value * d.quantity * d.units_sf)) total
+            FROM sales_detail d
+            JOIN sales s ON s.id=d.sale_id
+            JOIN users u ON u.id=s.responsible_id
+            GROUP BY 1
+            ORDER BY 3 desc";
+
+        $commercial = DB::select($sql);
+        $commercial = $commercial[0];
 
         if (Auth::user()->status_id == 3) {
             $users = Auth::user();
@@ -94,8 +92,8 @@ class HomeController extends Controller {
             if (Auth::user()->role_id == 2) {
                 return view('client', compact("product", "client", "supplier", "commercial"));
             } else {
-//                return view('dashboard', compact("product", "client", "supplier", "commercial"));
-                return view('dashboard');
+                return view('dashboard', compact("product", "client", "supplier", "commercial"));
+//                return view('dashboard');
             }
         }
     }
--- app/Http/Controllers/Sales/creditnoteController.php
+++ app/Http/Controllers/Sales/creditnoteController.php
@@ -103,7 +103,7 @@ class creditnoteController extends Controller {
 
         $sale = Sales::where("departure_id", $id)->first();
         $detail = DB::table("sales_detail")
-                ->select(DB::raw("sales_detail.quantity - credit_note_detail.quantity as quantity"), DB::raw("sales_detail.tax * 100 as tax"), DB::raw("coalesce(sales_detail.description,'') as description"), "products.title as product", "products.id as product_id", "sales_detail.value", "sales_detail.units_sf", DB::raw("sales_detail.units_sf * sales_detail.quantity as quantityTotal"), DB::raw("sales_detail.value * (sales_detail.quantity - credit_note_detail.quantity) * sales_detail.units_sf as valueTotal"), "stakeholder.business as stakeholder")
+                ->select(DB::raw("credit_note_detail.quantity as quantity"), DB::raw("sales_detail.tax * 100 as tax"), DB::raw("coalesce(sales_detail.description,'') as description"), "products.title as product", "products.id as product_id", "sales_detail.value", "sales_detail.units_sf", DB::raw("sales_detail.units_sf * sales_detail.quantity as quantityTotal"), DB::raw("sales_detail.value * (sales_detail.quantity - credit_note_detail.quantity) * sales_detail.units_sf as valueTotal"), "stakeholder.business as stakeholder")
                 ->join("products", "sales_detail.product_id", "products.id")
                 ->join("stakeholder", "products.supplier_id", "stakeholder.id")
                 ->join("credit_note_detail", "credit_note_detail.product_id", "sales_detail.product_id")

02b8242e470fd7eaa4b6a7eb294253692e141941
jorge <tech@superfuds.com.co>
2017-06-12 10:11:11 -0500

    quit panel in the table main

--- resources/views/Sales/CreditNote/list.blade.php
+++ resources/views/Sales/CreditNote/list.blade.php
@@ -1,45 +1,37 @@
 <div class="col-md-12">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <div class="row">
-                <div class="col-lg-3">List Departure</div>
-            </div>
 
-        </div>
-        <div class="panel-body">
-            <table class="table table-condensed  table-hover" id="tbl" width='100%'>
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>#</th>
-                        <th>Invoice</th>
-                        <th>Date</th>
-                        <th>Client</th>
-                        <th>Commercial</th>
-                        <th>Warehouse</th>
-                        <th>City</th>
-                        <th>Status</th>
-                        <th tipo="prueba">Factura</th>
-                        <th tipo="prueba">Nota credito</th>
-                    </tr>
-                </thead>
-                <tbody>
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <th></th>
-                        <th>#</th>
-                        <th>Date</th>
-                        <th>Client</th>
-                        <th>Commercial</th>
-                        <th>Warehouse</th>
-                        <th>City</th>
-                        <th>Status</th>
-                        <th></th>
-                        <th tipo="prueba">Nota credito</th>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    </div>
+    <table class="table table-condensed  table-hover" id="tbl" width='100%'>
+        <thead>
+            <tr>
+                <th></th>
+                <th>#</th>
+                <th>Invoice</th>
+                <th>Date</th>
+                <th>Client</th>
+                <th>Commercial</th>
+                <th>Warehouse</th>
+                <th>City</th>
+                <th>Status</th>
+                <th tipo="prueba">Factura</th>
+                <th tipo="prueba">Nota credito</th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th></th>
+                <th>#</th>
+                <th>Date</th>
+                <th>Client</th>
+                <th>Commercial</th>
+                <th>Warehouse</th>
+                <th>City</th>
+                <th>Status</th>
+                <th></th>
+                <th tipo="prueba">Nota credito</th>
+            </tr>
+        </tfoot>
+    </table>
+
 </div>

2d449f76194662c8b02d0abc9807e810dd7596a3
jorge <tech@superfuds.com.co>
2017-07-24 12:49:42 -0500

    fixed sum in purchases and add observavations

--- sql/views.sql
+++ sql/views.sql
@@ -48,7 +48,7 @@ LEFT JOIN parameters as status ON status.code=p.status_id and status."group"='ge
 
 
 create view vdepartures as 
-select d.id,coalesce(d.invoice,'') invoice, d.created_at, CASE WHEN s.business IS NULL THEN sta.business ELSE sta.business END client,w.description as warehouse,
+select d.id,coalesce(d.invoice,'') invoice,d.branch_id, d.created_at, CASE WHEN d.branch_id IS NULL THEN sta.business ELSE CASE WHEN s.business IS NULL THEN sta.business ELSE s.business END END client,w.description as warehouse,
             c.description as city,p.description status,d.status_id,d.responsible_id,u.name ||' '|| u.last_name as responsible,d.warehouse_id,
             (select coalesce(sum(quantity),0)::int from departures_detail where departure_id=d.id) quantity,
             

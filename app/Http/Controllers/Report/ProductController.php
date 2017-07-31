a969bf452b07ab32cbae5f9de7e6ad044a10dc36
jorge <tech@superfuds.com.co>
2017-07-31 17:28:37 -0500

    fixed units report

--- app/Http/Controllers/Report/ProductController.php
+++ app/Http/Controllers/Report/ProductController.php
@@ -39,14 +39,12 @@ class ProductController extends Controller {
 
 
         $sql = "
-            SELECT p.id,p.title as product,
-            sum(d.quantity * CASE  WHEN packaging=0 THEN 1 WHEN packaging IS NULL THEN 1 ELSE packaging END) totalunidades
-            ,round(sum(d.value * d.quantity * coalesce(d.units_sf))) as total
-            FROM sales_detail d
-            JOIN sales s ON s.id=d.sale_id
-            JOIN products p ON p.id=d.product_id  
+          SELECT p.id,p.title as product, sum(d.quantity * CASE WHEN d.packaging=0 THEN 1 WHEN d.packaging IS NULL THEN 1 ELSE d.packaging END) totalunidades ,round(sum(d.value * d.quantity * coalesce(d.units_sf))) as total 
+            FROM departures_detail d 
+            JOIN departures dep ON dep.id=d.departure_id and dep.status_id=2
+            JOIN products p ON p.id=d.product_id 
             WHERE product_id is not null
-            AND s.created BETWEEN'" . $input["init"] . " 00:00' AND '" . $input["end"] . " 23:59'
+            AND dep.created BETWEEN'" . $input["init"] . " 00:00' AND '" . $input["end"] . " 23:59'
             $where $ware
             GROUP by 1,2
             ORDER BY 3 DESC

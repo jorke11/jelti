2e0d6724d586802326cd89dee0e0592b92c94e18
jorge <tech@superfuds.com.co>
2017-08-02 12:31:36 -0500

    add

--- resources/views/Inventory/departure/newService.blade.php
+++ resources/views/Inventory/departure/newService.blade.php
@@ -27,6 +27,7 @@
                         </div>
                     </div>
                 </div>
+                {!!Form::close()!!}
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-danger" data-dismiss="modal" >Cerrar</button>

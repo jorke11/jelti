80083b390356b7f93553d4fdcf5e0bcffe4465e6
jorge <tech@superfuds.com.co>
2017-09-12 16:11:48 -0500

    add middleware to stock

--- app/Http/Controllers/Inventory/StockController.php
+++ app/Http/Controllers/Inventory/StockController.php
@@ -14,6 +14,10 @@ use App\Models\Administration\Warehouses;
 
 class StockController extends Controller {
 
+    public function __construct() {
+        $this->middleware("auth");
+    }
+
     public function index() {
         $warehouse = Warehouses::all();
         return view("Inventory.stock.init", compact("warehouse"));

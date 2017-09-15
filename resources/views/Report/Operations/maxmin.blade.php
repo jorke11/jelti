67b0f456de75a7c9ed9ad29324be145fa617a83f
jorge <tech@superfuds.com.co>
2017-09-15 12:25:58 -0500

    add reporte maximo

--- resources/views/Report/Operations/maxmin.blade.php
+++ resources/views/Report/Operations/maxmin.blade.php
@@ -8,7 +8,7 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Fletes</div>
+                <div class="panel-heading">Maximos</div>
                 <div class="panel-body container-row">
                     <table class="table nowrap" cellspacing="0" width="100%" id="tblmaxmin">
                         <thead>

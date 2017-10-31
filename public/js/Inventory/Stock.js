8e4c835b87097f5b58a543a3b27dd8e5ef4b0122
jorge pinedo <jpinedom@hotmail.com>
2017-10-31 11:46:18 -0500

    add

--- public/js/Inventory/Stock.js
+++ public/js/Inventory/Stock.js
@@ -13,6 +13,10 @@ function Stock() {
         param.warehouse_id = $("#warehouse_id").val();
         param.bar_code = $("#bar_code").val();
         return $('#tbl').DataTable({
+            "dom":
+                    "R<'row'<'col-sm-4'l><'col-sm-2 toolbar text-right'><'col-sm-3'B><'col-sm-3'f>>" +
+                    "<'row'<'col-sm-12't>>" +
+                    "<'row'<'col-xs-3 col-sm-3 col-md-3 col-lg-3'i><'col-xs-6 col-sm-6 col-md-6 col-lg-6 text-center'p><'col-xs-3 col-sm-3 col-md-3 col-lg-3'>>",
 //            processing: true,
 //            serverSide: true,
             destroy: true,
@@ -21,6 +25,13 @@ function Stock() {
                 url: "/api/listStock",
                 data: param
             },
+            buttons: [
+                {
+                    extend: 'excelHtml5',
+                    className: 'btn btn-primary glyphicon glyphicon-download',
+                    titleAttr: 'Excel'
+                },
+            ],
             columns: [
                 {data: "id"},
                 {data: "reference"},

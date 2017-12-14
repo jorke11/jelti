47f99d0acc9e896e8c3519b917a9365d3a46998f
jorge pinedo <jpinedom@hotmail.com>
2017-12-14 15:51:55 -0500

    add control execition modal per each twelve hour

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,3 +1,22 @@
+
+<?php
+# Iniciando la variable de control que permitirá mostrar o no el modal
+$exibirModal = false;
+# Verificando si existe o no la cookie
+if (!isset($_COOKIE["mostrarModal"])) {
+    # Caso no exista la cookie entra aquí
+    # Creamos la cookie con la duración que queramos
+    //$expirar = 3600; // muestra cada 1 hora
+    //$expirar = 10800; // muestra cada 3 horas
+    //$expirar = 21600; //muestra cada 6 horas
+    $expirar = 43200; //muestra cada 12 horas
+    //$expirar = 86400;  // muestra cada 24 horas
+    setcookie('mostrarModal', 'SI', (time() + $expirar)); // mostrará cada 12 horas.
+    # Ahora nuestra variable de control pasará a tener el valor TRUE (Verdadero)
+    $exibirModal = true;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -388,9 +407,9 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
         <br>
         </section>
 
-    
 
-   
+
+
 
 
         <section >
@@ -519,3 +538,14 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
         {!!Html::script('js/Page/page.js')!!}
 
         </html>
+        <?php if ($exibirModal === true) : // Si nuestra variable de control "$exibirModal" es igual a TRUE activa nuestro modal y será visible a nuestro usuario. ?>
+            <script>
+                $(document).ready(function ()
+                {
+                    // id de nuestro modal
+                    $("#myModal").modal("show");
+
+                });
+            </script>
+        <?php endif; ?>
+        

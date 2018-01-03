ca4899e6fde69284cfae456d7f53204922d91626
jorge pinedo <jpinedom@hotmail.com>
2018-01-03 05:30:52 -0500

    fixed padding in subcategory

--- resources/views/Ecommerce/shopping/specific.blade.php
+++ resources/views/Ecommerce/shopping/specific.blade.php
@@ -47,7 +47,7 @@
                                     </div>
                                 </div>
                                 <div class="item">
-                                    <div class="row">
+                                    <div class="row" style="padding-top: 1%;padding-bottom: 1%">
                                         <?php
                                     }
                                 }

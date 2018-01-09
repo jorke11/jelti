361ce480e6b22015aee387f6a746a563a4f8aeaa
jorge pinedo <jpinedom@hotmail.com>
2018-01-09 17:07:16 -0500

    finish blog

--- resources/views/Blog/content/init.blade.php
+++ resources/views/Blog/content/init.blade.php
@@ -86,7 +86,7 @@
                 <div class="caption" >
                     <p class="text-center"><img src="../images_blog/category_line/belleza.png" alt="" width="30%"></p>
                     <h3 class="text-center"><a href="{{url("blog/".$val["slug"])}}">{{$val["title"]}}</a></h3>
-                    <p style="min-height: 70px" class="text-center"><a href="{{url("blog/".$val["slug"])}}">{!!substr(strip_tags($val["content"]),0,150)."..."!!}</a></p>
+                    <p style="min-height: 70px" class="text-center"><a href="{{url("blog/".$val["slug"])}}">{!!substr(strip_tags(trim($val["content"])),0,150)."..."!!}</a></p>
                     <!--<p><a href="blog/{{$val["slug"]}}" class="btn btn-primary" role="button">Ver Mas</a>--> 
                     <p  class="text-center text-muted">Hace 1 minuto</p>
                 </div>

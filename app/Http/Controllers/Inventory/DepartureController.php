efde6a0bdcb8bd73bfb4ecc40aca4deb131bc87c
jorge pinedo <jpinedom@hotmail.com>
2018-01-29 11:50:42 -0500

    fixd load products with excel

--- app/Http/Controllers/Inventory/DepartureController.php
+++ app/Http/Controllers/Inventory/DepartureController.php
@@ -1551,6 +1551,7 @@ class DepartureController extends Controller {
                         if (isset($book->item) && $book->item != '') {
 
                             $special = PricesSpecial::where("item", (int) $book->item)->where("client_id", $this->in["client_id"])->first();
+//                            echo "<pre>";print_r($special);exit;
                             if ($special == null) {
                                 $product_id = 0;
                             } else {
@@ -1575,8 +1576,13 @@ class DepartureController extends Controller {
                                 $special = PricesSpecial::where("product_id", $pro->id)->where("client_id", $this->in["client_id"])->first();
                             }
                         } else {
+
                             if (isset($book->sf_code) && $book->sf_code != '') {
-                                $pro = Products::where("reference", $book->sf_code)->first();
+
+                                $pro = PricesSpecial::where("reference", (int) $book->sf_code)->where("client_id", $this->in["client_id"])->first();
+                                if ($pro == '') {
+                                    $pro = Products::where("reference", $book->sf_code)->first();
+                                }
                             }
                         }
 

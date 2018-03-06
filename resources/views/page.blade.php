e0fb3a4541ceebc8ca8592c4a97c3670a3ba2b07
jorge pinedo <jpinedom@hotmail.com>
2018-03-06 12:34:38 -0500

    add

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -365,7 +365,7 @@ if (!isset($_COOKIE["mostrarModal"])) {
 
             <div class="row row-space test">
                 <div class="col-lg-8 col-lg-offset-2 ">
-                    <div class="carousel slide media-carousel" id="subcategories">
+                    <div class="carousel slide media-carousel" id="subcategories-xs">
                         <div class="carousel-inner">
                             <div class="item  active">
                                 <div class="row row-center" style="padding-top: 2%;padding-bottom: 2%;">
@@ -403,11 +403,11 @@ if (!isset($_COOKIE["mostrarModal"])) {
                                 </div>
                             </div>
                         </div>
-                        <a class="left carousel-control" href="#subcategories" role="button" data-slide="prev">
+                        <a class="left carousel-control" href="#subcategories-xs" role="button" data-slide="prev">
                             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                         </a>
-                        <a class="right carousel-control" href="#subcategories" role="button" data-slide="next">
+                        <a class="right carousel-control" href="#subcategories-xs" role="button" data-slide="next">
                             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
                         </a>

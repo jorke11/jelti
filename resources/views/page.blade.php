5117be7aa9c09e67b74f45364e603c85e4337d38
jorge pinedo <jpinedom@hotmail.com>
2018-03-06 17:24:05 -0500

    add clear cache

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -24,7 +24,7 @@ clearstatcache();
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta http-equiv="Pragma" content="no-cache">
         <title>SuperFüds</title>
         <script>var PATH = '{{url("/")}}'</script>
         <link rel="shortcut icon" href="{{ asset('assets/images/logoico.png') }}">

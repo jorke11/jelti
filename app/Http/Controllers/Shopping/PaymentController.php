5b18c9880a7caed779ea627c49059904ef7155d8
jorge pinedo <jpinedom@hotmail.com>
2018-03-15 11:08:30 -0400

    add

--- app/Http/Controllers/Shopping/PaymentController.php
+++ app/Http/Controllers/Shopping/PaymentController.php
@@ -209,7 +209,7 @@ class PaymentController extends Controller {
                 WHERE order_id=" . $this->order->id . "
                 GROUP BY 1,2,3,4,d.units_sf,product_id,p.image,d.tax,p.thumbnail,d.value
                 ORDER BY 1";
-                
+                echo $sql;exit;
                 $detail = DB::select($sql);
 
                 $detail = json_decode(json_encode($detail), true);

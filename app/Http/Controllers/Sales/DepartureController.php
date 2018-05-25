361b73de20ff4405d8ad62d22dd3ef634993502a
jorge pinedo <jpinedom@hotmail.com>
2018-05-25 09:34:53 -0500

    test

--- app/Http/Controllers/Sales/DepartureController.php
+++ app/Http/Controllers/Sales/DepartureController.php
@@ -227,7 +227,7 @@ class DepartureController extends Controller {
                     ->first();
         }
 
-
+        dd($dep);
 
         $city_send = Cities::find($dep->destination_id);
         $city_inv = Cities::find($dep->city_id);

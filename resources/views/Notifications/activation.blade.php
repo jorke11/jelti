b2aed796232fb0b4afcf49daa25237f64465a226
jorge pinedo <jpinedom@hotmail.com>
2018-06-14 10:44:15 -0500

    add

--- resources/views/Notifications/activation.blade.php
+++ resources/views/Notifications/activation.blade.php
@@ -4,7 +4,7 @@
     </head>
     <body>
         <strong>Se activo tu usuario {!!$email!!} con la clave:<a href="#">{!!$password!!}</a></strong>
-        <p>Por favor ingresar al siguiente link: <a href="http://35.167.120.166/">Activación</a> para registrar sus datos,</p>
+        <p>Por favor ingresar al siguiente link: <a href="http://www.superfuds.com/">Activación</a> para registrar sus datos,</p>
         <p>Luego de registrarse, cerrar Sesion para usar el sistema</p>
     </body>
 </html>
